ea25a990589e2346b6ff0642ebce277ec57b162d
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-05-23 04:13:43 +0530

    setup webpack and basic API routes

---
+++ src/server/aylienAPI.js
@@ -0,0 +1,39 @@
+const dotenv = require('dotenv');
+const aylien = require("aylien_textapi");
+
+dotenv.config();
+
+// set aylien API credentias
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
+
+const getApiResponse = (url, fn) => {
+    textapi.sentiment({
+        'url': url
+      }, function(error, response) {
+        if (error === null) {
+        //   console.log(response);
+          fn(response);
+        }
+        return error;
+      });
+}
+
+module.exports = getApiResponse
+
+
+/*
+
+API response format
+
+{
+  polarity: 'positive',
+  subjectivity: 'objective',
+  text: 'John is a very good football player!',
+  polarity_confidence: 0.9940106272697449,
+  subjectivity_confidence: 0.943706847162803
+}
+
+*/
---
+++ src/server/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+const apiResponse = require('./aylienAPI.js');
+
+const app = express();
+const port = 8081;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('dist'))
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+app.post('/test', function (req, res) {
+    const url = req.body.url;
+    apiResponse(url, (response, writer = res)=>{
+        writer.send(response);
+    });
+})
+
+app.listen(port, ()=>{
+    console.log(`NLP app listening on port ${port}!`);
+})
